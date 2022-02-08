1d747613b8dbdc97a1ca2cbd1eb1dc7c0d75aaed
Temple <Templevoke@gmail.com>
2022-02-08 04:23:39 +0100

    added first htlm

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express")
 const app=express();
 app.listen(3000,()=>{
     console.log('the server is live');
 });
 app.use(express.static('public'));
+
+app.post('/api', (request,response) => {
+console.log(request);
+});
